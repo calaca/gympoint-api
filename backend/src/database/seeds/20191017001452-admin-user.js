0f7d51078d29ea51c1f50bee0d2c241ae36da4f4
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-16 21:18:24 -0300

    feat: add users table and seeds

---
+++ backend/src/database/seeds/20191017001452-admin-user.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Administrador',
+          email: 'administrador@gympoint.com',
+          password_hash: bcrypt.hashSync('senha123', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: queryInterface => {
+    return queryInterface.bulkDelete('users', null, {});
+  },
+};
