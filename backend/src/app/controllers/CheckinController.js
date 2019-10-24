4a38decf0b96f98a20c2c5e32b4123c63010232a
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-23 23:36:03 -0300

    feat: checkin index and store

--- backend/src/app/controllers/CheckinController.js
+++ backend/src/app/controllers/CheckinController.js
@@ -1,12 +1,57 @@
-// import Checkin from '../models/Checkin';
+import { subDays } from 'date-fns';
+import { Op } from 'sequelize';
+import Checkin from '../models/Checkin';
+import Student from '../models/Student';
 
 class CheckinController {
   async store(req, res) {
-    return res.json({ method: 'store' });
+    const { id } = req.params;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    const startDay = new Date();
+    const endDay = subDays(startDay, 6);
+
+    const checkins = await Checkin.findAll({
+      where: {
+        student_id: id,
+        updatedAt: {
+          [Op.between]: [endDay, startDay],
+        },
+      },
+    });
+
+    if (checkins.length >= 5) {
+      return res.status(400).json({
+        error: 'Student have already checked in during last 7 days',
+      });
+    }
+
+    // const checkin = await Checkin.create({ student_id: id });
+
+    return res.json(checkins);
   }
 
   async index(req, res) {
-    return res.json({ method: 'index' });
+    const { id } = req.params;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    const checkins = await Checkin.findAll({
+      where: {
+        student_id: id,
+      },
+    });
+
+    return res.json(checkins);
   }
 }
 
