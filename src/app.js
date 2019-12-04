67c1d99c55abebb3c92c137a1c199b26e0b45cf3
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-04 19:40:31 -0300

    feat: enable cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -12,6 +13,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
